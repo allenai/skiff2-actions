af8b0a0b5bae987734271d4488a712cc854f8ba7
Taylor Blanton <matthewtblanton@gmail.com>
2026-03-27 17:27:22 -0500

    Allow specifying cache-from and cache-to (#6)
    This PR allows action users to specify where the docker cache should
    pull from and push to. It makes testing easier and will enable github
    caching in the future. I saw this while changing the secrets code and
    wanted to change it since github actions caching is VERY nice.
    
    I'm tempted to change the defaults for the cache to the [Github Actions
    cache](https://docs.docker.com/build/ci/github-actions/cache/#github-cache)
    but decided to keep them the same for now.

---
+++ shared-actions/build/util.ts
@@ -0,0 +1,35 @@
+const CAPTURE_INSIDE_BRACES_REGEX = /\$\{(.*)\}/g;
+
+type Replacements = { [variableName: string]: string };
+
+/**
+ *
+ * @param valueToExpand A string with variables enclosed like ${VARIABLE}
+ * @param replacements An object with variable names as keys and the value to replace them with as the value
+ * @returns valueToExpand with specified variables expanded
+ * @example
+ * // returns 'Hello world!'
+ * expandVariables('Hello ${LOCATION_NAME}!', { LOCATION_NAME: 'world'})
+ */
+export function expandVariables(
+  valueToExpand: string,
+  replacements: Replacements,
+): string {
+  const bracesRegex = new RegExp(
+    Object.keys(replacements)
+      .map((replacementKey) => "\\$\\{" + replacementKey + "\\}")
+      .join("|"),
+    "g",
+  );
+
+  const expandedValue = valueToExpand.replace(bracesRegex, (matchedValue) => {
+    const replacementKey = matchedValue.replaceAll(
+      CAPTURE_INSIDE_BRACES_REGEX,
+      "$1",
+    ) as keyof typeof replacements;
+
+    return replacements[replacementKey];
+  });
+
+  return expandedValue;
+}
