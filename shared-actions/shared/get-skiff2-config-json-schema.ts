18b74a4c52f74c8c5932125ca2f50543e4a4e6fb
Taylor Blanton <matthewtblanton@gmail.com>
2026-04-01 17:03:14 -0500

    add json schema generation script

---
+++ shared-actions/shared/get-skiff2-config-json-schema.ts
@@ -0,0 +1,10 @@
+import path from "path";
+import { BuildConfigSchema } from "./skiff2-config.ts";
+import { writeFile } from 'fs/promises'
+
+const jsonSchema = JSON.stringify(BuildConfigSchema.toJSONSchema({io: "input"}))
+
+const filePath = path.join('skiff2-config.schema.json')
+await writeFile(filePath, jsonSchema)
+
+console.info(`JSON Schema created at ${path.resolve(filePath)}`)
