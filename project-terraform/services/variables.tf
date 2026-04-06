1ba51b121a81f8c591da0aabe17fdc721d4c8434
Taylor Blanton <matthewtblanton@gmail.com>
2026-04-06 16:22:13 -0500

    pr comments

--- project-terraform/services/variables.tf
+++ project-terraform/services/variables.tf
@@ -18,6 +18,7 @@ variable "services" {
     allow_unauthenticated    = bool
     allow_delete             = bool
     secret_files             = map(string)
+    use_http2                = bool
     custom_domains           = list(string)
     image_tag                = string
     deployment_environment   = string
