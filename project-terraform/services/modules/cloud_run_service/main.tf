078ce1bd3fdc5098f4ecb4f171175a69ba672439
Taylor Blanton <matthewtblanton@gmail.com>
2026-04-16 15:40:26 -0500

    allow users to set up secrets for all environments

--- project-terraform/services/modules/cloud_run_service/main.tf
+++ project-terraform/services/modules/cloud_run_service/main.tf
@@ -4,7 +4,7 @@
 data "google_secret_manager_secrets" "app_secrets" {
   for_each = toset([for key, container in var.service_containers : container.name])
 
-  filter = "name:${var.deployment_environment}-${each.value}-"
+  filter = "name:${each.value}-"
 }
 
 resource "google_cloud_run_v2_service" "service" {
@@ -87,7 +87,7 @@ resource "google_cloud_run_v2_service" "service" {
         # Secrets must be named "<ENV_VAR>-<service-name>".
         # The prefix and hyphens are stripped/converted to produce the env var name.
         dynamic "env" {
-          for_each = data.google_secret_manager_secrets.app_secrets[containers.value.name].secrets
+          for_each = [for secret in data.google_secret_manager_secrets.app_secrets[containers.value.name].secrets : secret if regex("^${var.deployment_environment}-${containers.value.name}|^${containers.value.name}-", secret.name)]
 
           content {
             name = trimprefix(env.value.secret_id, "${var.deployment_environment}-${containers.value.name}-")
