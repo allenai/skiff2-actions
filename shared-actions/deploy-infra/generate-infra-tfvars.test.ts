4e814824972f02ca1f209d71d53d89a9d0143a43
Taylor Blanton <matthewtblanton@gmail.com>
2026-05-14 13:44:12 -0700

    make a test for generate infra TF vars

---
+++ shared-actions/deploy-infra/generate-infra-tfvars.test.ts
@@ -0,0 +1,133 @@
+import { fs, vol } from "memfs";
+import { beforeEach, expect, test, vi } from "vitest";
+import type { BuildConfigInput } from "../shared/skiff2-config";
+import { stubGithubActionInput } from "../test-util/stub-github-action-input";
+import { generateInfraTFVars } from "./generate-infra-tfvars";
+
+vi.mock("node:fs");
+vi.mock("node:fs/promises");
+
+beforeEach(() => {
+  vol.reset();
+});
+
+const fakeConfig = {
+  projectName: "skiff2-test-build",
+  environments: ["main"],
+  services: [
+    {
+      name: "generate-infra-test",
+      cwd: ".",
+      dockerFile: "build-test.Dockerfile",
+      deploy: true,
+      isRootService: true,
+      allowUnauthenticated: true,
+      allowedPrincipals: ["domain:allenai.org"],
+      allowDelete: true,
+      secretFiles: {},
+      customDomains: ["foo.pandajungle.org"],
+      httpVersion: "2",
+      serviceAccount: "service.account@project.google.com",
+      includeDNSAuthorizationForExternalDomains: true,
+      machine: {
+        minInstances: 5,
+        maxInstances: 20,
+        memory: "2Gi",
+        cpu: 5,
+        cpuIdle: false,
+      },
+      startup: {
+        initialDelaySeconds: 60,
+        timeoutSeconds: 10,
+        periodSeconds: 30,
+        failureThreshold: 20,
+        path: "startup",
+        port: 3000,
+      },
+      liveness: {
+        initialDelaySeconds: 10,
+        timeoutSeconds: 20,
+        periodSeconds: 30,
+        path: "liveness",
+        port: 4000,
+      },
+      sidecars: [
+        {
+          name: "generate-service-test-sidecar",
+          cwd: "./sidecar",
+          dockerFile: "sidecar.Dockerfile",
+          secretFiles: {},
+          machine: {
+            memory: "512Mi",
+            cpu: 1,
+            cpuIdle: true,
+          },
+          startup: {
+            initialDelaySeconds: 1,
+            timeoutSeconds: 2,
+            periodSeconds: 3,
+            failureThreshold: 4,
+            path: "sidecar-startup",
+            port: 5,
+          },
+        },
+      ],
+    },
+    {
+      name: "custom-domain-without-dns-auth-test",
+      includeDNSAuthorizationForExternalDomains: false,
+      cwd: ".",
+      deploy: true,
+      customDomains: ["no-dns-auth.pandajungle.org"],
+    },
+  ],
+  remoteServices: [
+    {
+      name: "remote-service-with-dns-auth",
+      customDomains: ["remote-service-with-dns-auth.pandajungle.org"],
+      includeDNSAuthorizationForExternalDomains: true
+    },
+  ],
+} as const satisfies BuildConfigInput;
+
+test("generateInfraTFVars maps correctly", async () => {
+  stubGithubActionInput("config_file", "/fake-config-file.json");
+  stubGithubActionInput("project_id", "fake-skiff-project");
+  stubGithubActionInput("region", "fake-region");
+  stubGithubActionInput("use_classic_load_balancer", "false");
+
+  fs.writeFileSync("/fake-config-file.json", JSON.stringify(fakeConfig));
+
+  fs.mkdirSync("/terraform");
+  vi.stubEnv("TERRAFORM_DIR", "/terraform");
+
+  await generateInfraTFVars();
+
+  const fileContents = fs.readFileSync(
+    "/terraform/generated.auto.tfvars.json",
+    { encoding: "utf-8" },
+  ) as string;
+  const parsedFileContents = JSON.parse(fileContents);
+
+  expect(parsedFileContents).toEqual({
+    branch_environments: [],
+    custom_domain_mappings: {
+      "foo.pandajungle.org": {
+        include_dns_authorization_for_external_domains: true,
+        service_name: "prod-generate-infra-test",
+      },
+      "no-dns-auth.pandajungle.org": {
+        include_dns_authorization_for_external_domains: false,
+        service_name: "prod-custom-domain-without-dns-auth-test",
+      },
+      "remote-service-with-dns-auth.pandajungle.org": {
+        include_dns_authorization_for_external_domains: true,
+        service_name: "prod-remote-service-with-dns-auth",
+      },
+    },
+    default_service: "generate-infra-test",
+    project_id: "fake-skiff-project",
+    region: "fake-region",
+    use_classic_load_balancer: false,
+  });
+});
