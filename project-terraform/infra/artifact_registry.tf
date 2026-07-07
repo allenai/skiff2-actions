c1dd2304c7a1be1cf04208a4e27a6d90868cee2f
Taylor Blanton <matthewtblanton@gmail.com>
2026-07-07 15:47:56 -0500

    run the policies, turn off dry run

--- project-terraform/infra/artifact_registry.tf
+++ project-terraform/infra/artifact_registry.tf
@@ -4,7 +4,7 @@ resource "google_artifact_registry_repository" "gcr-io" {
   repository_id = "gcr.io"
   format        = "DOCKER"
 
-  cleanup_policy_dry_run = true
+  cleanup_policy_dry_run = false
   cleanup_policies {
     id     = "keep-minimum-versions"
     action = "KEEP"
